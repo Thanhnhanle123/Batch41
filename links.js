d1bce7db13c5a933699680b6ad5e9a829a21e578
nestact <86041474+Thanhnhanle123@users.noreply.github.com>
2026-05-31 22:53:38 +0700

    Add files via upload

--- links.js
+++ links.js
@@ -9,7 +9,14 @@ const LINKS = [
 
   {
     stt: 2,
-    keywords: ['hpo', 'đơn hàng', 'tạo hợp đồng', 'dashboard', 'quản lý đơn'],
+    keywords: [
+      'hpo',
+      'đơn hàng',
+      'tạo hợp đồng',
+      'dashboard',
+      'quản lý đơn',
+      'tạo đơn hàng'
+    ],
     name: 'HPO',
     url: 'https://hpo.hdsaison.com.vn/login?returnUrl=%2Fdashboard',
     note: 'Hệ thống quản lý đơn hàng của HD SAISON'
@@ -17,7 +24,7 @@ const LINKS = [
 
   {
     stt: 3,
-    keywords: ['mail', 'email', 'outlook', 'owa', 'thư'],
+    keywords: ['mail', 'email', 'outlook', 'owa', 'thư', 'hộp thư'],
     name: 'Email nội bộ',
     url: 'https://mail.hdsaison.com.vn/owa/auth/logon.aspx',
     note: 'Hệ thống email nội bộ của HD SAISON'
@@ -29,7 +36,8 @@ const LINKS = [
       'account',
       'authorization',
       'reset password',
-      'quản lý tài khoản'
+      'quản lý tài khoản',
+      'thay đổi mật khẩu'
     ],
     name: 'Quản lý tài khoản',
     url: 'https://quanlytk.hdsaison.com.vn:9251/authorization.do',
@@ -66,17 +74,8 @@ const LINKS = [
     url: 'https://elearning.hdsaison.com.vn/login/index.php',
     note: 'Hệ thống học tập trực tuyến của HD SAISON'
   },
-
   {
     stt: 8,
-    keywords: ['xuất cavet', 'cavet', 'rút cavet'],
-    name: 'Yêu cầu xuất cavet',
-    url: 'https://forms.office.com/pages/responsepage.aspx?id=v-ZoGBHKMUC4C0evO-91b2zwrFemGJ1HtKwpV4rW0tVUQTI0WkpKMjNQRUZUOFVLR1lKMVI5VTNEQy4u',
-    note: 'Hệ thống yêu cầu xuất cavet'
-  },
-
-  {
-    stt: 9,
     keywords: ['upload cavet', 'up cavet', 'tải cavet'],
     name: 'Upload cavet',
     url: 'https://forms.office.com/pages/responsepage.aspx?id=v-ZoGBHKMUC4C0evO-91b2zwrFemGJ1HtKwpV4rW0tVUOUNQOTdCN01PTlJWVEYxU0U4MFJIUEI1SS4u',
@@ -84,23 +83,23 @@ const LINKS = [
   },
 
   {
-    stt: 10,
-    keywords: ['hỗ trợ tín dụng', 'credit support', 'support tín dụng'],
+    stt: 9,
+    keywords: ['hỗ trợ tín dụng', 'credit support', 'support tín dụng', 'giải ngân', 'thư xác nhận'],
     name: 'Hỗ trợ tín dụng',
     url: 'https://forms.office.com/pages/responsepage.aspx?id=v-ZoGBHKMUC4C0evO-91b2zwrFemGJ1HtKwpV4rW0tVUOU1PRlBCREk2Q1VXMjFRVzRCSU4wRUM5VC4u',
     note: 'Hệ thống hỗ trợ tín dụng'
   },
 
   {
-    stt: 11,
-    keywords: ['update payment', 'thanh toán', 'cập nhật thanh toán'],
+    stt: 10,
+    keywords: ['update payment', 'thanh toán', 'cập nhật thanh toán', 'pending', 'điều chỉnh thanh toán', 'điều chỉnh'],
     name: 'Cập nhật thanh toán khách hàng',
     url: 'https://forms.office.com/pages/responsepage.aspx?id=v-ZoGBHKMUC4C0evO-91b8AAWMp0y69JimmTZeaMwMpUNjJPOFBWRDZNNUdBRTlVTUxSV0RJS1BTTS4u&route=shorturl',
     note: 'Hệ thống cập nhật thanh toán khách hàng'
   },
 
   {
-    stt: 12,
+    stt: 11,
     keywords: ['hr', 'nhân sự', 'salary', 'lương'],
     name: 'HR - Nhân sự',
     url: 'https://hr.hdsaison.com.vn/',
@@ -108,7 +107,7 @@ const LINKS = [
   },
 
   {
-    stt: 13,
+    stt: 12,
     keywords: ['bhyt', 'bảo hiểm', 'tra cứu bhyt'],
     name: 'Tra cứu BHYT',
     url: 'https://baohiemxahoi.gov.vn/tracuu/Pages/tra-cuu-thoi-han-su-dung-the-bhyt.aspx',
@@ -116,7 +115,7 @@ const LINKS = [
   },
 
   {
-    stt: 14,
+    stt: 13,
     keywords: ['gplx', 'bằng lái', 'giấy phép lái xe'],
     name: 'Tra cứu GPLX',
     url: 'https://gplx.csgt.bocongan.gov.vn/',
